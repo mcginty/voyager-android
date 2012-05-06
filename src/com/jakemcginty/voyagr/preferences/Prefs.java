87f3c316daf189e1a17c86475f4ee525832c868b
Jake McGinty <mcginty@illinois.edu>
2012-05-06 01:02:45 -0500

    new icons and preferences java file. also new android support for fragments.

---
+++ src/com/jakemcginty/voyagr/preferences/Prefs.java
@@ -0,0 +1,9 @@
+package com.jakemcginty.voyagr.preferences;
+
+public final class Prefs {
+	public static final String defaultPostURL = "http://jake.su/report";
+	//public static final String defaultPostURL = "http://10.0.2.2:8081/report";
+	public static final long defaultDuration = 0L;
+	public static final String broadcastAction = "com.jakemcginty.voyagr.VoyagrService.LOCATION_UPDATE";
+	public static final String prefsName = "VoyagerPrefs";
+}
